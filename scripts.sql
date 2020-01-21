c8478dda66f362ccc29d87d6334670a8659eff8b
mwbarnesy <mwbarnesy@gmail.com>
2020-01-21 13:00:42 -0600

    Question 11 complete!

--- scripts.sql
+++ scripts.sql
@@ -69,12 +69,11 @@ WHERE location = 'CA';
 all companies that have more than 5000 reviews across all locations. 
 How many companies are there with more that 5000 reviews across all locations? */
 
-
-	SELECT company, AVG(star_rating) AS avg_rating, review_count
-	FROM data_analyst_jobs
-	GROUP BY company, review_count
-	HAVING SUM(review_count) > 5000
-	;
+SELECT DISTINCT company, AVG(star_rating) AS avg_rating, review_count	
+FROM data_analyst_jobs
+GROUP BY company, review_count
+HAVING SUM(review_count) > 5000
+;
 
 /* Q10 Add the code to order the query in #9 from highest to lowest average star rating. 
 Which company with more than 5000 reviews across all locations in the dataset 
@@ -82,6 +81,12 @@ has the highest star rating? What is that rating? */
 
 /* Q11 Find all the job titles that contain the word ‘Analyst’. 
 How many different job titles are there? */
+
+SELECT COUNT(DISTINCT(title))
+FROM data_analyst_jobs
+WHERE title LIKE '%Analyst%';
+
+--754 different job titles
 
 /* Q12 How many different job titles do not contain either the word ‘Analyst’ 
 or the word ‘Analytics’? What word do these positions have in common? */
